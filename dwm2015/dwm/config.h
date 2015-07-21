3fe1139718a30a5ba4dc8fffed6ee90f81a5a786
brontosaurusrex <bronto@saurus.rex>
2015-07-21 11:04:03 +0200

    modified:   dwm2015/dwm/config.h

--- dwm2015/dwm/config.h
+++ dwm2015/dwm/config.h
@@ -28,7 +28,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",       NULL,       NULL,       0,            True,        -1 },
-	{ "Iceweasel",  NULL,       NULL,       1 << 2,       False,       -1 },
+/*	{ "Iceweasel",  NULL,       NULL,       1 << 2,       False,       -1 }, */
+
+	{ "Iceweasel",  NULL,       NULL,       0,            False,       -1 }, 
+
 };
 
 /* layout(s) */
